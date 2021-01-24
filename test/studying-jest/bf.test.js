c4a8b7f79eddfe82059d0d93e74559c485fed905
ahah12k <ahah12k@gmail.com>
2021-01-24 10:39:38 +0900

    im sick

--- test/studying-jest/bf.test.js
+++ test/studying-jest/bf.test.js
@@ -1,1 +1,17 @@
-//https://www.daleseo.com/jest-before-after/
+const userService=require('./bfFunction')
+const data=require('./bfData')
+
+test("find all users", () => {
+    data.users.push(
+        { id: 1, email: "user1@test.com" },
+        { id: 2, email: "user2@test.com" },
+        { id: 3, email: "user3@test.com" }
+    )
+
+    const users = userService.findAll()
+
+    expect(users).toHaveLength(3)
+    expect(users).toContainEqual({ id: 1, email: "user1@test.com" })
+    expect(users).toContainEqual({ id: 2, email: "user2@test.com" })
+    expect(users).toContainEqual({ id: 3, email: "user3@test.com" })
+})
