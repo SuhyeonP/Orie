b845a1ce52656bb62c210fbb93f1b822f8a02162
suhyeon <ahah12k@gmail.com>
2021-02-19 09:11:52 +0900

    asdf

--- chart/us-echarts/src/App.tsx
+++ chart/us-echarts/src/App.tsx
@@ -5,42 +5,39 @@ import TestComponent from "./rb/TestComp";
 import useStore from "./store";
 
 const App=():JSX.Element=>{
-  const status:string[]=['Ready','Loading','Train','deploy',"fail",'Pause','normal',"abnormal"];
-  const statusButtonOrder: number[] = [7, 6, 0, 1, 2, 3, 4, 5];
-  const [filterArray,setFilterArray]=useState<any[]>(dummy)// todo  타이핑은 robot arm interface 폴더내부에 타이핑 정의한거 불러 쓰면될듯
 
 
-    const { filterStore } = useStore();
-    const { filterArray2 } = filterStore;
+    const {filterStore, robotListStore} = useStore();
+    const {filterArray2} = filterStore;
+    const {robotList} = robotListStore;
+    const status: string[] = ['Ready', 'Loading', 'Train', 'deploy', "fail", 'Pause', 'normal', "abnormal"];
+    const statusButtonOrder: number[] = [7, 6, 0, 1, 2, 3, 4, 5];
+    const [filterArray, setFilterArray] = useState<any[]>(dummy)// todo  타이핑은 robot arm interface 폴더내부에 타이핑 정의한거 불러 쓰면될듯
 
 
-    // useEffect(()=>{
-    //     if(!filterArray2.includes(false)){
-    //         console.log('print all')
-    //     }
-    // })
     //todo 처음엔 all false인데, 선택하면 true로 되니까 이후에 all true, all false는 다 보여주는거
 
-  const filterButton=(statusValue:number)=>{
-      filterStore.setFilterArray(statusValue)
-      // if(!filterArray2.includes(false)){
-      //     console.log(' be all true')
-      // }else{
-      //     console.log("lets filtering")
-      // }
-      //todo every 메서드 함수 사용하면 배열내의 값이 all true 인지 all false 인지 알수있음 배열.every(x=>x===true) 이런식
+    const filterButton = useCallback((statusValue: number) => {
+        filterStore.setFilterArray(statusValue)
+        // if(!filterArray2.includes(false)){
+        //     console.log(' be all true')
+        // }else{
+        //     console.log("lets filtering")
+        // }
+        //todo every 메서드 함수 사용하면 배열내의 값이 all true 인지 all false 인지 알수있음 배열.every(x=>x===true) 이런식
 
-        console.log(filterArray2)
-      // console.log(statusValue)
-      // console.log(robots.filter(a=>a.status===statusValue))
-      // setFilterArray(prev=>{
-      //     prev.push(robots.filter(a=>a.status===statusValue).map(x=>x.anomalyScores.map(y=>y.output[0].anomaly_score)))
-      //     return prev;
-      // })// mst이용해서?
-      console.log(filterArray2)
+        if (filterArray2.every(x => x === true) || filterArray2.every(x => x === false)) {
+            console.log('all')
+            robotListStore.setRobotListReset();
+        } else {
+            robotListStore.setRobotList(statusValue)
+        }
+
+        setFilterArray(robotList)
 
 
-  }
+
+    }, [robotList])
     //todo statusArray를 변환하는건 됫고 이후에 그걸 바탕으로 true면 보여주는거? 안보여주는거... ;ㅅ;
   return (
     <div className="App">
@@ -49,12 +46,15 @@ const App=():JSX.Element=>{
                 <TestComponent chartData={x} chartId={dd.result.robots[y].name}/>
             </div>
         )}
-      {filterArray.length}
-          <div>
-            {status.map((x,y)=>(
-                <button key={y} onClick={()=>filterButton(statusButtonOrder[y])}>{status[statusButtonOrder[y]]}:{robots.filter(a=>a.status===(statusButtonOrder[y])).length}</button>
+        {filterArray.length}
+        <div>
+            {status.map((x, y) => (
+                <button key={y}
+                        onClick={() => filterButton(statusButtonOrder[y])}>{status[statusButtonOrder[y]]}:{robots.filter(a => a.status === (statusButtonOrder[y])).length}</button>
             ))}
-          </div>
+        </div>
+        <br/>
+        <br/>
     </div>
   );
 }
--- chart/us-echarts/src/store/index.ts
+++ chart/us-echarts/src/store/index.ts
@@ -1,7 +1,9 @@
 import filterStore, { FilterStore } from './filter';
+import robotListStore, {RobotListStore} from "./robot";
 
 const useStore = (): {
-    filterStore: FilterStore
-} => ({ filterStore });
+    filterStore: FilterStore,
+    robotListStore:RobotListStore
+} => ({ filterStore,robotListStore });
 
 export default useStore;
